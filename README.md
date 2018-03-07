619109f304c78c729ac861543ce02ada910eccf4
root <root@ip-172-31-28-142.us-west-2.compute.internal>
2018-03-07 04:31:26 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# aws-eb-helloworld-nodejs
+
+This is a project to show you how to Deploy a Node.js Application like [this](https://github.com/GoogleCloudPlatform/nodejs-getting-started.git) over [AWS Beanstalk](https://aws.amazon.com/elasticbeanstalk/) using [AWS Cloud Formation](https://aws.amazon.com/cloudformation).
+
+## Prerequisites
+
+To run this code, you need:
+* An [AWS Account](https://aws.amazon.com) and an [user with AWS Access Key](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html)
+* Python and Python Pip installed on your local computer
+
+## Install Local Requirements
+
+### AWS Command Line Interface (AWS CLI)
+
+Installing the [AWS Command Line Interface](https://aws.amazon.com/cli)
+```
+pip install --upgrade --user awscli
+```
+
+Configure awscli (iterative mode)
+```
+cd ~
+aws configure
+```
+
+### Elastic Beanstalk Command Line Interface (EB CLI)
+
+Install the [Elastic Beanstalk Command Line Interface](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3.html)
+```
+pip install --upgrade --user awsebcli
+```
+
+### Create key pairs for AWS Beanstalk EC2 instances
+
+Creating a [AWS Key Pairs using AWSCLI](http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-using-cli.html)
+```
+aws ec2 create-key-pair \
+  --key-name hello-world-app-ec2-key-pair \
+  --query 'KeyMaterial' \
+  --region eu-west-1 \
+  --output text > ~/.ssh/hello-world-app-ec2-key-pair.pem
+
+chmod 400 ~/.ssh/hello-world-app-ec2-key-pair.pem
+```
+
+## Create initial stack
+
+This project provide a [deploy.sh](deploy.sh) script to help us deploy our project and environment.
+
+```
+./deploy.sh init
+```
+
+It use AWS CloudFormation template locate in [deployment/cf-beanstalk.json](deployment/cf-beanstalk.json)
+and a config file [deployment/helloworld-conf.json](deployment/helloworld-conf.json) where you can put your own config Parameters
+
+## Destroy stack
+
+This project provide a [deploy.sh](deploy.sh) script to help us destroy our project and environment.
+
+```
+./deploy.sh destroy
+```
+
+## Deploying changes
+
+you can use [eb cli](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb3-cmd-commands.html) to control  your project.
+
+if you want to changes your project, execute:
+
+```
+eb deploy <your environment name>
+```
+
+if you want to know the environments in your project, just execute:
+
+```
+eb list
+```
+
+if you want open your application environment using your default web browser use:
+
+```
+eb open
+```
+
+if you want to knoe more about [eb cli, click here](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb3-cmd-commands.html)
+
+## References
+
+* [Passing Parameters to CloudFormation Stacks with the AWS CLI and Powershell](https://aws.amazon.com/es/blogs/devops/passing-parameters-to-cloudformation-stacks-with-the-aws-cli-and-powershell/)
+* [AWS Elastic Beanstalk --> Supported Platforms](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts.platforms.html#concepts.platforms.nodejs)
+* [AWS Elastic Beanstalk --> General Options for All Environments](http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html#command-options-general-elasticbeanstalkhealthreporting)
+* [AWS CloudFormation --> Sample Templates](http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/sample-templates-services-us-west-2.html#w2ab2c21c48c13c29)
+* [Develop, Deploy, and Manage for Scale with Elastic Beanstalk and CloudFormation Series](https://aws.amazon.com/es/blogs/devops/part-1-develop-deploy-and-manage-for-scale-with-elastic-beanstalk-and-cloudformation-series/)
---
+++ deploy.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+################################################################################
+#
+#
+# enable
+set -e
+
+# enable debug
+#set -x
+
+################################################################################
+# Parameters
+CF_TEMPLATE_FILE="./deployment/cf-beanstalk.json"
+CF_CONFIG_FILE="./deployment/helloworld-conf.json"
+REPO_NAME=`basename $(git remote show -n origin | grep Push | cut -d: -f2- | cut -d\. -f2)`
+PROGNAME=$(basename $0)
+
+################################################################################
+# Validate arguments
+if [ $# -lt 1 ]
+then
+  echo "Usage: ${PROGNAME} init|destroy"
+  exit 1
+fi
+
+ARG=$1
+
+################################################################################
+# Functions
+function file_exist() {
+  local name="$1"
+  if [ -f $CF_TEMPLATE ] ; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+function get_param() {
+  local name="$1"
+  cat $CF_CONFIG_FILE | jq -r --arg name $name '.[] | select(.ParameterKey == $name) | .ParameterValue'
+}
+
+function bucket_exist() {
+    local bucket="$1"
+    local region="$2"
+    if aws s3 ls s3://$bucket --region $region 2>&1 | grep -q "NoSuchBucket"; then
+      echo false
+    else
+      echo true
+    fi
+}
+
+function cf_stack_exist() {
+    local name="$1"
+    local region="$2"
+    if aws cloudformation describe-stacks \
+      --stack-name ${name} \
+      --region ${region} 2>&1 | grep -q "ValidationError"; then
+      echo false
+    else
+      echo true
+    fi
+}
+
+function cf_create_stack() {
+  local name="$1"
+  local template="$2"
+  local config_file="$3"
+  local region="$4"
+  aws cloudformation create-stack \
+    --stack-name $name \
+    --template-body file://$template \
+    --parameters file://$config_file \
+    --capabilities CAPABILITY_IAM \
+    --on-failure DELETE \
+    --region $region
+}
+
+function cf_delete_stack() {
+  local name="$1"
+  local region="$2"
+  aws cloudformation delete-stack \
+    --stack-name $name \
+    --region $region
+}
+
+function create_bucket() {
+  local name="$1"
+  local region="$2"
+  aws s3api create-bucket \
+    --bucket $name \
+    --region $region \
+    --create-bucket-configuration LocationConstraint=$region
+}
+
+function delete_bucket() {
+  local name="$1"
+  local region="$2"
+  aws s3 rm s3://$name --recursive --region $region
+}
+
+function wait_until_cf_stack_creation() {
+  local name="$1"
+  local region="$2"
+  echo "Waiting until Cloudformation stack \"${name}\" is created, takes a long time!"
+  aws cloudformation wait stack-create-complete \
+    --stack-name $name \
+    --region $region
+  echo "Cloudformation stack \"${name}\" was created"
+}
+
+function upload_artifact() {
+  local bucket="$1"
+  local key="$2"
+  local region="$3"
+  git archive --format zip HEAD | aws s3 cp - s3://$bucket/$key --region $region
+}
+
+function eb_init() {
+  local region="$1"
+  eb init --platform node.js --region $region
+}
+
+function eb_use() {
+  local env="$1"
+  eb use $env
+}
+
+################################################################################
+
+CF_STACK_NAME=$(get_param ApplicationName)
+BUCKET=$(get_param ApplicationS3Bucket)
+KEY=$(get_param ApplicationS3Artifact)
+REGION=$(get_param AWSRegion)
+BUCKET_EXIST=$(bucket_exist ${BUCKET} ${REGION})
+CF_STACK_EXIST=$(cf_stack_exist ${CF_STACK_NAME} ${REGION})
+CF_TEMPLATE_FILE_EXIST=$(file_exist $CF_TEMPLATE_FILE)
+CF_CONFIG_FILE_EXIST=$(file_exist $CF_CONFIG_FILE)
+STACK_ENVIRONMENT=$(get_param EnvironmentName)
+
+if [[ "${CF_TEMPLATE_FILE_EXIST}" == false ]]; then
+  echo "${CF_TEMPLATE_FILE} is necessary"
+fi
+
+if [[ "${CF_CONFIG_FILE_EXIST}" == false ]]; then
+  echo "${CF_CONFIG_FILE} is necessary"
+fi
+
+#
+if [[ "${ARG}" == "init" ]]; then
+
+  # Create a BUCKET if not exist
+  if [[ "${BUCKET_EXIST}" == false ]]; then
+    create_bucket $BUCKET $REGION
+  fi
+
+  # upload our artifact to S3
+  upload_artifact $BUCKET $KEY $REGION
+
+  if [[ "${CF_STACK_EXIST}" == false ]]; then
+    cf_create_stack $CF_STACK_NAME $CF_TEMPLATE_FILE $CF_CONFIG_FILE $REGION
+    wait_until_cf_stack_creation $CF_STACK_NAME $REGION
+    eb_init $REGION
+    eb_use $STACK_ENVIRONMENT
+    cp deployment/cloudwatch.config .ebextensions/
+    #eb deploy
+  fi
+
+elif [[ "${ARG}" == "destroy" ]]; then
+
+  if [[ "${CF_STACK_EXIST}" == true ]]; then
+    cf_delete_stack $CF_STACK_NAME $REGION
+  fi
+
+  # Create a BUCKET if not exist
+  if [[ "${BUCKET_EXIST}" == true ]]; then
+    delete_bucket $BUCKET $REGION
+  fi
+
+else
+  echo "Invalid argument"
+fi
